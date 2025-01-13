f632dfc66ad10d25d912cee6c6e2375550fdb1c9
Anshmodi03 <modiaastha01@gmail.com>
2025-01-13 19:59:03 +0530

    updated file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: "https://parknest.vercel.app", // Replace with your frontend URL
     methods: ["GET", "POST"],
   },
 });
@@ -18,7 +18,7 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://parknest.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
