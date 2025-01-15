cdb9a31d811036f783f05e7647bd75d2c0697bd0
Anshmodi03 <modiaastha01@gmail.com>
2025-01-15 13:45:05 +0530

    update

--- client/src/components/Slots/LoadingScreen.jsx
+++ client/src/components/Slots/LoadingScreen.jsx
@@ -1,10 +1,12 @@
-// LoadingScreen.jsx
 import React from "react";
 import BarLoader from "../Loader/BarLoader.jsx";
 
 const LoadingScreen = () => (
-  <div className="flex justify-center items-center min-h-screen">
+  <div className="flex flex-col justify-center items-center min-h-screen">
     <BarLoader />
+    <p className="text-sm text-yellow-300 mt-4 text-center">
+      If stuck at loading, please refresh the website.
+    </p>
   </div>
 );
 
